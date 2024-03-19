4a2041475d2236096759e44cc7a838fef8b8693a
Nitro Cao <jaycecao520@gmail.com>
2024-03-19 11:32:16 +0800

    fix(java): don't use slf4j in bootstrap package to solve NoSLF4JProviders error in engine package

--- javaagent/bootstrap/src/main/java/org/nitroc/javaagent/bootstrap/App.java
+++ javaagent/bootstrap/src/main/java/org/nitroc/javaagent/bootstrap/App.java
@@ -1,27 +1,21 @@
 package org.nitroc.javaagent.bootstrap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.instrument.Instrumentation;
 
 public class App {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-
-
     public static void premain(String args, Instrumentation inst) throws Exception {
-        logger.info("[DemoAgent] in premain method");
-        logger.info("[DemoAgent] agent args: {}", args);
+        System.out.println("[TOYRASP] in premain method");
+        System.out.printf("[TOYRASP] agent args: %s%n", args);
 
         EngineLoader.load(inst);
     }
 
     public static void agentmain(String args, Instrumentation inst) {
-        logger.info("[DemoAgent] in agentmain method");
+        System.out.println("[TOYRASP] in agentmain method");
 //        inst.addTransformer(new Transformer(), true);
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("[TOYRASP] Hello World!");
     }
 }
